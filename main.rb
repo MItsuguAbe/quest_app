e0ed21c2c38004ac4830739658a1ee250ebdf54b
MItsuguAbe <kouiy42@gmail.com>
2020-02-22 04:32:00 +0900

    モンスターの変身メソッド

--- main.rb
+++ main.rb
@@ -43,24 +43,56 @@ end
 
 class Monster
     attr_accessor :name, :hp, :offense, :defense
+
+    POWER_UP_RATE = 1.5
+    CALC_HALF_HP = 0.5
   
     def initialize(**params)
       @name = params[:name]
       @hp = params[:hp]
       @offense = params[:offense]
       @defense = params[:defense]
+
+      # モンスターが変身したかどうかを判定するフラグ
+      @transform_flag = false
+
+      # 変身する際の閾値（トリガー）を計算
+      @trigger_of_transform = params[:hp] * CALC_HALF_HP
     end
 
     def attack(brave)
+        if @hp <= @trigger_of_transform && @transform_flag == false
+            # モンスター変身判定フラグにtrueを代入
+            @transform_flag = true
+            # 変身メソッドを実行
+            transform
+        end
 
         puts "#{@name}の攻撃"
     
         damage = @offense - brave.defense
-        
+
         brave.hp -= damage
     
         puts "#{brave.name}は#{damage}のダメージを受けた"
         puts "#{brave.name}の残りHPは#{brave.hp}だ"
     end
+
+    
+    private
+
+      def transform
+       transform_name = "ドラゴン"
+
+        puts <<~EOS
+        #{@name}は怒っている
+        #{@name}は#{transform_name}に変身した
+        EOS
+
+        # モンスターの攻撃力を1.5倍にする
+        @offense *= POWER_UP_RATE
+        # モンスターの名前を変更
+        @name = transform_name
+      end
   
 end
